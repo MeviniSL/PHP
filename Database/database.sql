7888a94e3a5f7ed0832832ca6e8fb1822a464e69
Mevini Silva <silvamevini19@gmail.com>
2025-12-18 10:58:55 +0530

    create user table schema

--- Database/database.sql
+++ Database/database.sql
@@ -1,2 +1,11 @@
 CREATE DATABASE internship_db;
 USE internship_db;
+
+
+CREATE TABLE Users (
+    id INT AUTO-INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(10) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
